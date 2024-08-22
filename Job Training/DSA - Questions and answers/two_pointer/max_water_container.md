5f8e371253d1d9db6b1a45849168376cd09dc8a5
yohan john <yohanalexjohn@gmail.com>
2024-08-22 19:50:43 +0100

    max water

--- Job Training/DSA - Questions and answers/two_pointer/max_water_container.md
+++ Job Training/DSA - Questions and answers/two_pointer/max_water_container.md
@@ -17,3 +17,36 @@ Output: 36
 Input: height = [2,2,2]
 
 Output: 4
+
+```python
+# Brute force
+def solution(heights: List[int]) -> int:
+    volume = 0
+    start = 0
+
+    if heights == []:
+        return 0
+
+    end = len(heights) - 1
+
+    while start < end:
+        max_height = min(heights[start], heights[end])
+        max_width = end - start
+
+        volume = max(volume, max_height * max_width)
+
+        if heights[start] < heights[end]:
+            start += 1
+
+        else:
+            end -= 1
+
+    return volume
+
+
+print(solution([1, 7, 2, 5, 4, 7, 3, 6]))
+print(solution([2, 2, 2]))
+print(solution([]))
+print(solution([0]))
+```
+
--- Job Training/test.py
+++ Job Training/test.py
@@ -1,73 +1,27 @@
 from typing import DefaultDict, List
 
 
-def solution(nums: List[int]) -> List[List[int]]:
-    ans = []
-    temp = []
+def solution(heights: List[int]) -> int:
+    volume = 0
+    start = 0
+    end = len(heights) - 1
 
-    # Sort the input list
-    nums.sort()
+    while start < end:
+        max_height = min(heights[start], heights[end])
+        max_width = end - start
 
-    # If the input list has no data return empty
-    if nums == []:
-        return ans
+        volume = max(volume, max_height * max_width)
 
-    # Algorithm:
-    # For each increment of the number list. We use
-    # two pointers start and end to check if there is
-    # a valid match of the sum to get to 0
-    for i in range(len(nums)):
-        # if the sorted number list has no numbers in
-        # the negative list then exit with an empty list
-        if nums[i] > 0:
-            break
+        if heights[start] < heights[end]:
+            start += 1
 
-        # Remove the repeated iterative index as the
-        # answer will be similar to the ones before
-        if (i > 0) and (nums[i - 1] == nums[i]):
-            continue
-
-        # Start and Stop pointers
-        start = i + 1
-        end = len(nums) - 1
-
-        # For the value nums[i]. Find the rest of the numbers
-        # by using two pointer algorithm if the sum is greater
-        # than 0 reduce the end index and vice - versa.
-        # Increment start and decrement end if there is a match
-        while start < end:
-            if (nums[i] + nums[start] + nums[end]) > 0:
-                end -= 1
-
-            elif (nums[i] + nums[start] + nums[end]) < 0:
-                start += 1
-
-            else:
-                ans.append([nums[i], nums[start], nums[end]])
-
-                start += 1
-                end -= 1
-
-                continue
-
-    # Remove duplicate answers from the list
-    # Prefer seprate as the answers should be less than the
-    # calculation
-    for i in range(len(ans)):
-        if (i > 0) and (ans[i - 1] == ans[i]):
-            continue
         else:
-            temp.append(ans[i])
+            end -= 1
 
-    ans.clear()
-    ans = temp.copy()
-
-    return ans
+    return volume
 
 
-print(solution([-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6]))
-print(solution([-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4]))
-print(solution([-2, 0, 0, 2, 2]))
-print(solution([-1, 0, 1, 2, -1, -4]))
-print(solution([0, 0, 0]))
-print(solution([0, 1, 1]))
+print(solution([1, 7, 2, 5, 4, 7, 3, 6]))
+print(solution([2, 2, 2]))
+print(solution([]))
+print(solution([0]))
